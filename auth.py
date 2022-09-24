0f235458bc45fbbfb6632d2c83f9c3e59f717fdb
Nathaniel Simmons <nlsimmons42@gmail.com>
2022-09-24 18:26:34 +0800

    Update

---
+++ auth.py
@@ -0,0 +1,155 @@
+import sqlite3, requests, json
+from sqlite3 import Error
+from pprint import pprint
+from datetime import datetime, timezone, timedelta
+
+api_key = "SUdMkOldiHZ69WlZBwteQlMCgJtYLKoX"
+api_secret = "eS0DRC5jRuasfK6Z"
+
+
+def get_timestamp():
+     now = datetime.now(tz=timezone.utc)
+     return now.isoformat().split('.')[0]
+
+def select(**kwargs):
+     conn = sqlite3.connect(r"./db.db")
+     cursor = conn.cursor()
+
+     _fields = kwargs.get('fields', '*')
+     _table = kwargs.get('table')
+     _where = kwargs.get('where', [1])
+
+     if _fields == '*':
+          select = '*'
+     else:
+          fields = []
+          for f in _fields:
+               fields.append('`' + f + '`')
+          select = ','.join(fields)
+     where = ' AND '.join(_where)
+
+     query = f"""SELECT {select} FROM `{_table}` WHERE {where}"""
+     cursor.execute(query)
+
+     output = cursor.fetchall()
+
+     conn.commit()
+     conn.close()
+     try:
+          return output[0]
+     except IndexError:
+          return []
+
+
+def insert(**kwargs):
+     _table = kwargs.get('table')
+     _data = kwargs.get('data')
+     conn = sqlite3.connect(r"./db.db")
+
+     columns = []
+     rows = []
+
+     if not isinstance(_data, list):
+          _data = [_data]
+
+     for col in _data[0]:
+          columns.append(col)
+
+     for row in _data:
+          _row = []
+          for col in columns:
+               _row.append(row[col])
+          rows.append(_row)
+
+     quoted_columns = map(lambda i: f"`{i}`", columns)
+     query_columns = ','.join(quoted_columns)
+     query_rows = []
+     for row in rows:
+          quoted_items = map(lambda i: f"'{i}'", row)
+          query_rows.append("(" + ','.join(quoted_items) + ")")
+
+     query = f"""INSERT INTO `{_table}` ({query_columns}) VALUES {','.join(query_rows)}"""
+     conn.execute(query)
+
+     conn.commit()
+     conn.close()
+
+
+def get_stored_auth():
+     return select(fields=['access_token'],
+                   table='auth',
+                   where=[f"expires_at > '{get_timestamp()}'"])[0]
+
+# def get_stored_auth():
+#      conn = sqlite3.connect(r"./db.db")
+#      cursor = conn.cursor()
+#      cursor.execute(f"""SELECT access_token FROM `auth` WHERE expires_at > '{get_timestamp()}' LIMIT 1""")
+
+#      output = cursor.fetchall()
+
+#      conn.commit()
+#      conn.close()
+
+#      return output[0][0]
+
+def get_new_auth():
+     url = "https://test.api.amadeus.com/v1/security/oauth2/token"
+     headers = {
+          "Content-Type": "application/x-www-form-urlencoded"
+     }
+     params = {
+          "grant_type": "client_credentials",
+          "client_id": api_key,
+          "client_secret": api_secret
+     }
+
+     r = requests.post(url, data=params, headers=headers)
+     response = r.json()
+
+     now = datetime.now(tz=timezone.utc)
+     expires_at = now + timedelta(seconds=response['expires_in'])
+
+     expires_at = expires_at.isoformat().split('.')[0]
+     client_id = response['client_id']
+     access_token = response['access_token']
+
+     data = {
+          "client_id": client_id,
+          "access_token": access_token,
+          "expires_at": expires_at
+     }
+     insert(table='auth',
+            data=data)
+
+     return access_token
+
+
+# def get_new_auth():
+#      url = "https://test.api.amadeus.com/v1/security/oauth2/token"
+#      headers = {
+#           "Content-Type": "application/x-www-form-urlencoded"
+#      }
+#      params = {
+#           "grant_type": "client_credentials",
+#           "client_id": api_key,
+#           "client_secret": api_secret
+#      }
+
+#      r = requests.post(url, data=params, headers=headers)
+#      response = r.json()
+#      conn = sqlite3.connect(r"./db.db")
+
+#      now = datetime.now(tz=timezone.utc)
+#      expires_at = now + timedelta(seconds=response['expires_in'])
+
+#      expires_at = expires_at.isoformat().split('.')[0]
+#      client_id = response['client_id']
+#      access_token = response['access_token']
+
+#      conn.execute(f"""INSERT INTO `auth` (client_id, access_token, expires_at) VALUES ('{client_id}', '{access_token}', '{expires_at}')""")
+
+#      conn.commit()
+#      conn.close()
+
+#      return response
+
--- main.py
+++ main.py
@@ -1,22 +1,54 @@
-import sqlite3, requests
+import sqlite3, requests, json
 from sqlite3 import Error
+from pprint import pprint
+from datetime import datetime, timezone
+from auth import *
 
-url = "test.api.amadeus.com"
-api_key = "hmIQI2iCWjsIngmMrLsNgZYnm0u6YS3Y"
-api_secret = "C2mAhApDP3DO2YDJ"
+def get_timestamp():
+    now = datetime.now(tz=timezone.utc)
+    return now.isoformat().split('.')[0]
 
-def create_connection(db_file):
-    """ create a database connection to a SQLite database """
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-        print(sqlite3.version)
-    except Error as e:
-        print(e)
-    finally:
-        if conn:
-            conn.close()
+url = "http://test.api.amadeus.com"
+api_key = get_stored_auth()
+url = "https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=MNL&destinationLocationCode=BKK&departureDate=2022-11-01&adults=1&nonStop=false&max=250"
 
+headers = {
+    "accept": "application/vnd.amadeus+json",
+    "Authorization": f"Bearer {api_key}"
+}
 
-if __name__ == '__main__':
-    create_connection(r"./db.db")
+r = requests.get(url, headers=headers)
+response = r.json()
+
+for flight in response.get('data'):
+    for segment in flight['itineraries'][0]['segments']:
+        departure = segment['departure']['at']
+        arrival = segment['arrival']['at']
+        depart_code = segment['departure']['iataCode']
+        arrive_code = segment['arrival']['iataCode']
+        airline = segment['carrierCode']
+        aircraft = segment['aircraft']['code']
+
+        where = [
+            f"departure = '{departure}'",
+            f"arrival = '{arrival}'",
+            f"airline = '{airline}'",
+            f"depart_code = '{depart_code}'",
+            f"arrive_code = '{arrive_code}'",
+        ]
+        existing = select(fields=['id'], table='flights', where=where)
+        if len(existing) > 0:
+            pass
+        else:
+            data = {
+                "test": 1,
+                "departure": departure,
+                "arrival": arrival,
+                "depart_code": depart_code,
+                "arrive_code": arrive_code,
+                "airline": airline,
+                "aircraft": aircraft,
+                "last_updated": get_timestamp()
+            }
+            insert(table='flights',
+                   data=data)
